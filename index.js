fdff64b51422b99871a568d1b0e1736195cc1b3a
Noah Fairbairn <noahfairbairn@gmail.com>
2019-07-09 16:37:04 -0700

    first attempt at a nav bar

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ function getTrainer(id) {
       }
     })
     .then(res => res.json())
-    .then(json => saveTeamInfo(json))
+    .then(json => {
+      saveTeamInfo(json)
+      getNav(json)
+    })
 }
 
 function saveTeamInfo(trainerObject) {
@@ -142,7 +145,7 @@ function displayPokemonImage(pokemonObject, htmlElement) {
 function addTrainerButton() {
   const header = document.querySelector('header')
   const button = document.createElement('button')
-  header.textContent = ''
+  // header.textContent = ''  
   button.textContent = 'Trainer Info'
 
   button.addEventListener('click', () => displayTrainerInfo(1))
@@ -293,7 +296,133 @@ function createNewTeam(htmlElement, trainerObject) {
   })
   .then(res => res.json())
   .then(json => {
+    getNav(trainerObject)
     htmlElement.value = ''
     appendTeam(json, list, main)
   })
 }
+
+function listNavTeams(trainerObject){
+  let teamList = document.getElementById("teams");
+
+  let teams = trainerObject.teams
+
+  for(team of teams) {
+    displayNavTeams(team);
+  }
+}
+
+function displayNavTeams(team){
+  const main = document.querySelector('main')
+  let teamList = document.getElementById("teams");
+    let h4 = document.createElement("h4");
+        h4.textContent = team.name;
+        h4.id = team.id;
+
+        h4.addEventListener("click", () => {
+          closeNav();
+          displayTeamInfo(team, main)
+        })
+
+        h4.addEventListener("mouseenter", () => {
+          let pokemon = team.pokemon;
+          let ul = document.createElement("ul");
+
+          for (poke of pokemon){
+            displayPokemon(poke, ul);
+          }
+          h4.appendChild(ul);
+        })
+
+
+        h4.addEventListener("mouseleave", () => {
+          h4.children[0].remove();
+        })
+
+        teamList.appendChild(h4);
+}
+
+function displayPokemon(pokemonObject, htmlElement){
+    let li = document.createElement("li");
+      li.textContent = pokemonObject.name;
+
+      li.addEventListener("click", () => {
+        closeNav();
+        showSinglePokemon(pokemonObject);
+      })
+    htmlElement.appendChild(li);
+}
+
+// function displaySinglePokemon(pokemonObject, teamObject, list)
+
+// displayTeamInfo(teamObject, htmlElement)
+
+function clearNavTeams(trainerObject){
+  let teamList = document.getElementById("teams");
+
+  teamList.textContent = "";
+}
+
+function getNav(trainerObject){
+  clearNavTeams(trainerObject);
+  listNavTeams(trainerObject);
+}
+
+const closeButton = document.querySelector(".closebtn");
+
+  closeButton.addEventListener("click", () => {
+    closeNav();
+  })
+
+  const hamSpan = document.querySelector("#ham-span");
+
+    hamSpan.addEventListener("click", () => {
+      openNav();
+    })
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("hamburger-main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("hamburger-main").style.marginLeft= "0";
+}
+
+
+// const team1 = document.getElementById("team-1");
+
+//   team1.addEventListener("mouseenter", () => {
+//     dropDown();
+//   })
+
+//   team1.addEventListener("mouseleave", () => {
+//     team1.firstElementChild.remove();
+//   })
+
+
+
+// function dropDown(){
+//     // console.log(team1.children);
+//     let team1 = document.getElementById("team-1");
+//     let ul = document.createElement("ul");
+
+//     let li1 = document.createElement("li");
+//         li1.textContent = "pokemon 1";
+
+//         li1.addEventListener("click", () => {
+//           console.log("take me to the first page!")
+//         })
+    
+//     let li2 = document.createElement("li");
+//         li2.textContent = "pokemon 2";
+        
+//         li2.addEventListener("click", () => {
+//           console.log("take me to the second page!")
+//         })
+
+//     ul.appendChild(li1);
+//     ul.appendChild(li2);
+//     team1.appendChild(ul);
+// }
