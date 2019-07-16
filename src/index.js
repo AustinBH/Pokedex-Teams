46fc82627710cacb6995961a45993c827511f6ff
Austin Harlow <46305121+AustinBH@users.noreply.github.com>
2019-07-15 18:27:32 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const MAIN = document.getElementById("main-wrapper");
-const MAIN_URL = 'https://pokedex-yeet.herokuapp.com/'
+const MAIN_URL = 'https://pokedex-yeet.herokuapp.com'
 
 // Waiting for DOM to render login
 document.addEventListener('DOMContentLoaded', () => {
